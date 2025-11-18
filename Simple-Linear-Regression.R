aa932238e68e9c5b9b920f7a31ecbf2912fce410
Mojayd <142050561+Mojayd@users.noreply.github.com>
2025-11-18 16:46:34 -0500

    Add files via upload

---
+++ Simple-Linear-Regression.R
@@ -0,0 +1,14 @@
+#Author: David Mojekwu; Date: Aug 28, 2025; Purpose: Test linear regression
+
+#Get some dummy dataset
+cars <- mtcars
+
+#Scatter plot on training data
+scatter.smooth(x=cars$mpg, y=cars$disp, main="SpeedVSdistance")
+
+#Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+#Split training and testing data for regression
+training_data <- cars[1:16,]
+test_data <- cars[17:32,]
